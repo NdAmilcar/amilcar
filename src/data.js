cde000851fa30a0d8154328dd80b81b7c4fc6b01
NdAmilcar <ndamilcar@gmail.com>
2022-01-28 13:17:59 -0300

    update

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://www.xtrafondos.com/wallpapers/resized/paisaje-digital-en-atardecer-5846.jpg?s=large",
+    link: "https://ndamilcar.github.io/websiteA/",
+  },
+  {
+    id: 2,
+    img: "https://cdn.pixabay.com/photo/2021/04/13/23/25/paisaje-6176882_960_720.jpg",
+    link: "https://ndamilcar.github.io/websiteA/",
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/photos/woman-looking-at-many-pictures-picture-id165615541?k=20&m=165615541&s=612x612&w=0&h=FAF3QPVUa9-pEgQ9llZrlcDkstPhYAkCbw24oOZa6tg=",
+    link: "https://ndamilcar.github.io/websiteA/",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "https://ndamilcar.github.io/websiteA/",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "https://ndamilcar.github.io/websiteA/",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "https://ndamilcar.github.io/websiteA/",
+  },
+];
